56b514662223c61ae043be48a8c7ac7a7e3428f2
emmanueljose <emjose@gmail.com>
2022-09-11 00:55:44 -0400

    service worker test

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ function swRegistration() {
 	const heart = ["font-size: 20px", "padding: 12px", "margin: 4px 0 4px 4px", "color: rgba(238,58,136,1)"].join(";");
 	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker
-			.register("https://emjose.github.io/calculator-vuejs/sw.js", {
-				scope: "/",
+			.register("/calculator-vuejs/sw.js", {
+				scope: "/calculator-vuejs/",
 			})
 			.then(function (registration) {
 				console.log("%c❤️", heart);
